82ced89f30a4b6197c055130da005381079631d0
fukumaco <fukuasako@gmail.com>
2023-09-11 15:45:30 +0900

    表紙とGOALの目標を入力する画面を作成 目標を入力して保存する機能が欲しい。

---
+++ mamoru/GoalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GoalViewController.swift
+//  mamoru
+//
+//  Created by Fukumoto Asako on 2023/09/11.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- mamoru/ViewController.swift
+++ mamoru/ViewController.swift
@@ -5,15 +5,66 @@
 //  Created by Fukumoto Asako on 2023/09/11.
 //
 
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by Fukumoto Asako on 2023/08/26.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController , UITextViewDelegate{
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        textView.delegate = self
+        
+        
+        // SceneDelegateが持っている前回の内容を呼び出してTextViewに表示されるようにします。
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
+        
+    }
+     //TextViewに何か入力されたら動作します
+    func textViewDidChange(_ textView: UITextView) {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+//        textView.text = sceneDelegate.lastText
+        sceneDelegate.lastText = textView.text
+    
+    }
+    
+    
+    //textView.text = ""
+    
+
+}
+
+/*
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mamoru: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
 }
+
+*/
+
 
