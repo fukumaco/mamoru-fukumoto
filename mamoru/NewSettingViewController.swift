30fdef5a244d438e2c8d318e6e78ac8e9ce5ed43
fukumaco <fukuasako@gmail.com>
2023-09-14 13:25:22 +0900

    直してエラーもなく実行できるのですが、同じところで落ちてしまいます！どうすればいいのでしょうか？

--- mamoru/NewSettingViewController.swift
+++ mamoru/NewSettingViewController.swift
@@ -4,60 +4,77 @@
 //
 //  Created by Fukumoto Asako on 2023/09/12.
 //
+
+// NewSettingViewController
+
 import UIKit
 
 class NewSettingViewController: UIViewController {
-
+    
     //締切日時を入力
     @IBOutlet weak var deadline: UIDatePicker!
     //開始日時を入力
     @IBOutlet weak var startDate: UIDatePicker!
-
+    
     //平日のみか全日程か
     @IBOutlet weak var workDay: UISegmentedControl!
     //一日あたり何時間働くか
     @IBOutlet weak var hoursOfWorkPerDay: UITextField!
-
+    
     //残日数を表示
     @IBOutlet weak var remainingDays: UITextField!
     //残時間を表示
-     @IBOutlet weak var remainingTime: UITextField!
-
+    @IBOutlet weak var remainingTime: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
     //入力後、計算するボタン
     @IBAction func culculate(_ sender: Any) {
         let calendar = Calendar.current
-
+        
         let start = startDate.date
         let end = deadline.date
-
+        
         let components = calendar.dateComponents([.day, .hour], from: start, to: end)
-
+        
         let days = components.day ?? 0
         let hours = components.hour ?? 0
-
+        
         remainingDays.text = "\(days) days"
         remainingTime.text = "\(hours) hours"
     }
     
-    @IBAction func ToTodolistButton(_ sender: Any) {//segueを実行する
-        performSegue(withIdentifier: "showToDoInput", sender: self)
+    @IBAction func ToTodolistButton(_ sender: Any) {
+        
+        // segueを実行する
+        //performSegue(withIdentifier: "showToDoInput", sender: self)
+        // ToDoInputViewControllerのインスタンスを作成
+        let toDoInputVC = storyboard?.instantiateViewController(withIdentifier: "ToDoInputViewController") as! ToDoInputViewController
+        // 残日数と残時間を渡す
+        toDoInputVC.remainingDaysText = remainingDays.text ?? ""
+        toDoInputVC.remainingTimeText = remainingTime.text ?? ""
+        // 遷移する（セグエのIDは"showToDoInput"）
+        performSegue(withIdentifier: "showToDoInput", sender: toDoInputVC)
     }
-
-    //segueが実行される前に呼ばれる関数
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //segueのIdentifierが"showToDoInput"であれば
-        if segue.identifier == "showToDoInput" {
-            //遷移先のToDoInputViewControllerを取得する
-            let toDoInputVC = segue.destination as! ToDoInputViewController
-            //遷移先の残日数と残時間に値を渡す
-            toDoInputVC.remainingDays.text = remainingDays.text
-            toDoInputVC.remainingTime.text = remainingTime.text
-        }
-    }
+    
 }
+
+
+/*
+ //segueが実行される前に呼ばれる関数
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ //segueのIdentifierが"showToDoInput"であれば
+ if segue.identifier == "showToDoInput" {
+ //遷移先のToDoInputViewControllerを取得する
+ let toDoInputVC = segue.destination as! ToDoInputViewController
+ //遷移先の残日数と残時間に値を渡す
+ toDoInputVC.remainingDays.text = remainingDays.text
+ toDoInputVC.remainingTime.text = remainingTime.text
+ }
+ }
+ */
+
+
+
--- mamoru/ToDoInputViewController.swift
+++ mamoru/ToDoInputViewController.swift
@@ -4,33 +4,73 @@
 //
 //  Created by Fukumoto Asako on 2023/09/13.
 //
-
 import UIKit
 
 class ToDoInputViewController: UIViewController {
-    @IBOutlet weak var remainingDays: UITextField!
-    //残時間を表示
-     @IBOutlet weak var remainingTime: UITextField!
-
-
+    // 残日数と残時間を受け取るプロパティ（String型）
+    var remainingDaysText = ""
+    var remainingTimeText = ""
+    
+    @IBOutlet weak var remainingDays: UITextField!//残日を表示
+    @IBOutlet weak var remainingTime: UITextField!     //残時間を表示
+    @IBOutlet weak var inputText: UITextView!  //テキスト入力
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
         //残日数と残時間を表示する
             remainingDays.text = remainingDays.text
             remainingTime.text = remainingTime.text
-
+        
     }
-    
-        /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) UIImage(systemName: "ipad.homebutton"){
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //入力を保存する
+    @IBAction func saveButton(_ sender: Any) {
+        // UserDefaultsのインスタンスを作成
+        let userDefaults = UserDefaults.standard
+        // 入力したテキストをキーと値のペアで保存する（キーは"inputText"と仮定）
+        userDefaults.set(inputText.text, forKey: "inputText")
+        // UserDefaultsに反映させる
+        userDefaults.synchronize()
     }
-    */
-
+    //次のページへ遷移
+    @IBAction func ToDoListEditorButton(_ sender: Any) {
+    }
 }
+
+/*
+ // ToDoInputViewController
+ import UIKit
+
+ class ToDoInputViewController: UIViewController {
+     // 残日数と残時間を受け取るプロパティ（String型）
+     var remainingDaysText = ""
+     var remainingTimeText = ""
+     
+     // 残日数と残時間を表示するテキストフィールド
+     @IBOutlet weak var remainingDays: UITextField!
+     @IBOutlet weak var remainingTime: UITextField!
+     
+     // テキスト入力するテキストビュー
+     @IBOutlet weak var inputText: UITextView!
+     
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // テキストフィールドに残日数と残時間を表示する
+         remainingDays.text = remainingDaysText
+         remainingTime.text = remainingTimeText
+     }
+     
+     // 入力を保存するボタンが押された時の処理
+     @IBAction func saveButton(_ sender: Any) {
+         // UserDefaultsのインスタンスを作成
+         let userDefaults = UserDefaults.standard
+         // 入力したテキストをキーと値のペアで保存する（キーは"inputText"と仮定）
+         userDefaults.set(inputText.text, forKey: "inputText")
+         // UserDefaultsに反映させる
+         userDefaults.synchronize()
+     }
+     
+     // 次のページへ遷移するボタンが押された時の処理（省略）
+ }
+ */
