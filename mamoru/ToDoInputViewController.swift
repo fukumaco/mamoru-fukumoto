e80e0b66557fb66d286f0840c914173f8a177cab
fukumaco <fukuasako@gmail.com>
2023-09-14 14:23:30 +0900

    ToDoInputViewControllerに新しく作った変数を表示させるように修正したのですが、実行しても表示されません

--- mamoru/ToDoInputViewController.swift
+++ mamoru/ToDoInputViewController.swift
@@ -18,10 +18,10 @@ class ToDoInputViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
-        //残日数と残時間を表示する
-            remainingDays.text = remainingDays.text
-            remainingTime.text = remainingTime.text
+                //残日数と残時間を表示する
+                // プロパティの値をテキストフィールドに代入する
+                remainingDays.text = remainingDaysText
+                remainingTime.text = remainingTimeText
         
     }
         //入力を保存する
@@ -33,44 +33,9 @@ class ToDoInputViewController: UIViewController {
         // UserDefaultsに反映させる
         userDefaults.synchronize()
     }
+    
+    
     //次のページへ遷移
     @IBAction func ToDoListEditorButton(_ sender: Any) {
     }
 }
-
-/*
- // ToDoInputViewController
- import UIKit
-
- class ToDoInputViewController: UIViewController {
-     // 残日数と残時間を受け取るプロパティ（String型）
-     var remainingDaysText = ""
-     var remainingTimeText = ""
-     
-     // 残日数と残時間を表示するテキストフィールド
-     @IBOutlet weak var remainingDays: UITextField!
-     @IBOutlet weak var remainingTime: UITextField!
-     
-     // テキスト入力するテキストビュー
-     @IBOutlet weak var inputText: UITextView!
-     
-     override func viewDidLoad() {
-         super.viewDidLoad()
-         // テキストフィールドに残日数と残時間を表示する
-         remainingDays.text = remainingDaysText
-         remainingTime.text = remainingTimeText
-     }
-     
-     // 入力を保存するボタンが押された時の処理
-     @IBAction func saveButton(_ sender: Any) {
-         // UserDefaultsのインスタンスを作成
-         let userDefaults = UserDefaults.standard
-         // 入力したテキストをキーと値のペアで保存する（キーは"inputText"と仮定）
-         userDefaults.set(inputText.text, forKey: "inputText")
-         // UserDefaultsに反映させる
-         userDefaults.synchronize()
-     }
-     
-     // 次のページへ遷移するボタンが押された時の処理（省略）
- }
- */
